6d79971453e6ace8c803def46c117e8883dc66e6
wangchijie <905260054@qq.com>
2022-12-06 23:40:12 +0800

    '删除'

--- IT_Heima/01-08基础/chaoxing.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementsByClassName("orientationright ");
-setInterval(function () {
-    btn.click();
-},10000);
-
-setInterval(function () {
-    $('#right3').click();
-    $('#right2').click();
-},5000);
